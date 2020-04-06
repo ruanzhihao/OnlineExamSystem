7d3df37efb6dc9e73b6443653095afe21e807105
skzd <63005848+skzd@users.noreply.github.com>
2020-04-06 18:30:32 +0800

    sun

--- src/main/java/com/onlineExam/controller/LoginController.java
+++ src/main/java/com/onlineExam/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.onlineExam.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
@@ -9,4 +10,11 @@ public class LoginController {
     public String Login(){
         return "index";
     }
+
+    @RequestMapping("/test2")
+    public String Test2(){
+        return "Test2";
+    }
+
+
 }
