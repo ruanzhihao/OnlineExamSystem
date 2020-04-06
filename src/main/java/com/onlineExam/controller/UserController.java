2ae96a8d71a5d644da759c49909a81f882d13eca
skzd <63005848+skzd@users.noreply.github.com>
2020-04-06 10:26:42 +0800

    孙

--- src/main/java/com/onlineExam/controller/UserController.java
+++ src/main/java/com/onlineExam/controller/UserController.java
@@ -15,7 +15,7 @@ public class UserController {
     @RequestMapping(value = "test")
     public String getTestRes(Model model){
         /*model.addAttribute("test","测试");*/
-        return "index";
+        return "Test";
     }
     @RequestMapping("getUser")
     @ResponseBody
