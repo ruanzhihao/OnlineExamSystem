1cb8ae9e98c5d4a2d17ff17139293fe65ee92136
skzd <63005848+skzd@users.noreply.github.com>
2020-04-14 22:15:48 +0800

    sun

---
+++ src/main/java/com/onlineExam/domain/State.java
@@ -0,0 +1,10 @@
+package com.onlineExam.domain;
+
+import lombok.Data;
+
+
+@Data
+public class State {
+    private int stateId;
+    private String state;
+}
---
+++ src/main/java/com/onlineExam/domain/Student1.java
@@ -0,0 +1,23 @@
+package com.onlineExam.domain;
+
+import lombok.Data;
+
+
+@Data
+public class Student1 {
+    private Integer id;
+    private String username;
+    private Integer stuid;
+    private String stuname;
+    private String stupassword;
+    private String stuphonenumber;
+    private String stuemail;
+    private int majorId;
+    private String majorName;
+    private int departId;
+    private String departName;
+    private int stateId;
+    private String state;
+    private int clazzId;
+    private String clazzName;
+}
